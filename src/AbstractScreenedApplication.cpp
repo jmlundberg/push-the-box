6f7263418b024430f0871b0176ad3962e6550d21
Vladimír Vondruš <mosra@centrum.cz>
2012-11-19 15:03:58 +0100

    Screen management.
    To make it possible (and convenient) to have menu screen, game screen
    etc.

---
+++ src/AbstractScreenedApplication.cpp
@@ -0,0 +1,95 @@
+#include "AbstractScreenedApplication.h"
+
+#include <Framebuffer.h>
+
+#include "AbstractScreen.h"
+
+namespace PushTheBox {
+
+AbstractScreenedApplication* AbstractScreenedApplication::addScreen(AbstractScreen* screen) {
+    if(frontScreen()) frontScreen()->blurEvent();
+    insert(screen, frontScreen());
+    screen->focusEvent();
+    return this;
+}
+
+AbstractScreenedApplication* AbstractScreenedApplication::removeScreen(AbstractScreen* screen) {
+    screen->blurEvent();
+    erase(screen);
+    return this;
+}
+
+AbstractScreenedApplication* AbstractScreenedApplication::focusScreen(AbstractScreen* screen) {
+    /* Already focused, nothing to do */
+    if(frontScreen() == screen) return this;
+
+    frontScreen()->blurEvent();
+    move(screen, frontScreen());
+    screen->focusEvent();
+    return this;
+}
+
+void AbstractScreenedApplication::viewportEvent(const Math::Vector2<GLsizei>& size) {
+    Framebuffer::setViewport({}, size);
+
+    for(AbstractScreen* s = first(); s; s = s->next())
+        s->viewportEvent(size);
+}
+
+void AbstractScreenedApplication::drawEvent() {
+    /* Back-to-front rendering */
+    for(AbstractScreen* s = backScreen(); s; s = s->nextNearerScreen())
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Draw) s->drawEvent();
+}
+
+void AbstractScreenedApplication::keyPressEvent(KeyEvent& event) {
+    /* Front-to-back event propagation, stop when the event gets accepted */
+    for(AbstractScreen* s = frontScreen(); s; s = s->nextFartherScreen()) {
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Input) {
+            s->keyPressEvent(event);
+            if(event.isAccepted()) break;
+        }
+    }
+}
+
+void AbstractScreenedApplication::keyReleaseEvent(KeyEvent& event) {
+    /* Front-to-back event propagation, stop when the event gets accepted */
+    for(AbstractScreen* s = frontScreen(); s; s = s->nextFartherScreen()) {
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Input) {
+            s->keyReleaseEvent(event);
+            if(event.isAccepted()) break;
+        }
+    }
+}
+
+void AbstractScreenedApplication::mousePressEvent(MouseEvent& event) {
+    /* Front-to-back event propagation, stop when the event gets accepted */
+    for(AbstractScreen* s = frontScreen(); s; s = s->nextFartherScreen()) {
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Input) {
+            s->mousePressEvent(event);
+            if(event.isAccepted()) break;
+        }
+    }
+}
+
+void AbstractScreenedApplication::mouseReleaseEvent(MouseEvent& event) {
+    /* Front-to-back event propagation, stop when the event gets accepted */
+    for(AbstractScreen* s = frontScreen(); s; s = s->nextFartherScreen()) {
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Input) {
+            s->mouseReleaseEvent(event);
+            if(event.isAccepted()) break;
+        }
+    }
+}
+
+void AbstractScreenedApplication::mouseMoveEvent(MouseMoveEvent& event) {
+    /* Front-to-back event propagation, stop when the event gets accepted */
+    for(AbstractScreen* s = frontScreen(); s; s = s->nextFartherScreen()) {
+        if(s->propagatedEvents() & AbstractScreen::PropagatedEvent::Input) {
+            s->mouseMoveEvent(event);
+            if(event.isAccepted()) break;
+        }
+    }
+}
+
+}
---
+++ src/AbstractScreenedApplication.h
@@ -0,0 +1,162 @@
+#ifndef PushTheBox_AbstractScreenedApplication_h
+#define PushTheBox_AbstractScreenedApplication_h
+
+/** @file
+ * @brief Class PushTheBox::AbstractScreenedApplication
+ */
+
+#include <Containers/LinkedList.h>
+#include <Magnum.h>
+
+#ifndef MAGNUM_TARGET_NACL
+#include <Platform/Sdl2Application.h>
+#else
+#include <Platform/NaClApplication.h>
+#endif
+
+#include "PushTheBox.h"
+
+namespace PushTheBox {
+
+class AbstractScreen;
+
+/**
+@brief Base for applications with screen management
+
+Manages list of screens and propagates events to them.
+
+Traversing through the list of screens is done like following:
+@code
+// front-to-back
+for(AbstractScreen* s = app.frontScreen(); s; s = s->nextFartherScreen()) {
+    // ...
+}
+
+// back-to-front
+for(AbstractScreen* s = app.backScreen(); s; s = s->nextNearerScreen()) {
+    // ...
+}
+@endcode
+
+@section AbstractScreenedApplication-event-propagation Event propagation
+
+Each AbstractScreen specifies which set of events should be propagated to it
+using AbstractScreen::setPropagatedEvents(). When application gets an event,
+they are propagated to the screens:
+
+ - @ref Magnum::Platform::Sdl2Application::viewportEvent() "viewportEvent()"
+   is propagated to all screens.
+ - @ref Magnum::Platform::Sdl2Application::drawEvent() "drawEvent()" is
+   propagated in back-to-front order to screens which have
+   @ref AbstractScreen::PropagatedEvent "AbstractScreen::PropagatedEvent::Draw"
+   enabled.
+ - Input events (@ref Magnum::Platform::Sdl2Application::keyPressEvent() "keyPressEvent()",
+   @ref Magnum::Platform::Sdl2Application::keyReleaseEvent() "keyReleaseEvent()",
+   @ref Magnum::Platform::Sdl2Application::mousePressEvent() "mousePressEvent()",
+   @ref Magnum::Platform::Sdl2Application::mouseReleaseEvent() "mouseReleaseEvent()"
+   and @ref Magnum::Platform::Sdl2Application::mouseMoveEvent() "mouseMoveEvent()")
+   are propagated in front-to-back order to screens which have
+   @ref AbstractScreen::PropagatedEvent "AbstractScreen::PropagatedEvent::Input"
+   enabled. If any screen sets the event as accepted, it is not propagated
+   further.
+
+*/
+class AbstractScreenedApplication: public ApplicationBase, private Corrade::Containers::LinkedList<AbstractScreen> {
+    friend class Corrade::Containers::LinkedList<AbstractScreen>;
+    friend class Corrade::Containers::LinkedListItem<AbstractScreen, AbstractScreenedApplication>;
+    friend class AbstractScreen;
+
+    public:
+        /** @seemagnum{Platform::Sdl2Application::Sdl2Application()} */
+        #ifndef MAGNUM_TARGET_NACL
+        inline AbstractScreenedApplication(int argc, char** argv, const std::string& title = "Magnum screened application", const Math::Vector2<GLsizei>& size = Math::Vector2<GLsizei>(800, 600)): ApplicationBase(argc, argv, title, size) {}
+        #else
+        inline AbstractScreenedApplication(PP_Instance instance, const Math::Vector2<GLsizei>& size = Math::Vector2<GLsizei>(640, 480)): ApplicationBase(instance, size) {}
+        #endif
+
+        /**
+         * @brief Add screen to application
+         * @return Pointer to self (for method chaining)
+         *
+         * Previously focused screen is blurred and the new screen is focused.
+         * @see AbstractScreen::blur(), AbstractScreen::focus()
+         */
+        AbstractScreenedApplication* addScreen(AbstractScreen* screen);
+
+        /**
+         * @brief Remove screen from application
+         * @return Pointer to self (for method chaining)
+         *
+         * The screen is blurred before removing. Deleting the object is left
+         * up to the user.
+         * @see AbstractScreen::blur()
+         */
+        AbstractScreenedApplication* removeScreen(AbstractScreen* screen);
+
+        /**
+         * @brief Focus screen
+         * @return Pointer to self (for method chaining)
+         *
+         * Moves the screen to front. Previously focused screen is blurred and
+         * this screen is focused.
+         * @see AbstractScreen::blur(), AbstractScreen::focus()
+         */
+        AbstractScreenedApplication* focusScreen(AbstractScreen* screen);
+
+        /**
+         * @brief Front screen
+         *
+         * @see AbstractScreen::nextFartherScreen(), AbstractScreen::nextNearerScreen()
+         */
+        inline AbstractScreen* frontScreen() { return first(); }
+        inline const AbstractScreen* frontScreen() const { return first(); } /**< @overload */
+
+        /**
+         * @brief Back screen
+         *
+         * @see AbstractScreen::nextFartherScreen(), AbstractScreen::nextNearerScreen()
+         */
+        inline AbstractScreen* backScreen() { return last(); }
+        inline const AbstractScreen* backScreen() const { return last(); } /**< @overload */
+
+    protected:
+        /**
+         * @brief Viewport event
+         *
+         * Called when viewport size changes. You should pass the new size to
+         * Framebuffer::setViewport() and then call this function to propagate
+         * the event to all screens.
+         * @seemagnum{Platform::Sdl2Application::viewportEvent()}
+         */
+        void viewportEvent(const Math::Vector2<GLsizei>& size) override = 0;
+
+        /**
+         * @brief Draw event
+         *
+         * Called when the screen is redrawn. You should clean the framebuffer
+         * using Framebuffer::clear(). and then call this function to
+         * propagate the event to all screens. After the drawing is finished,
+         * call swapBuffers().
+         * @seemagnum{Platform::Sdl2Application::drawEvent()}
+         */
+        void drawEvent() override = 0;
+
+        /** @seemagnum{Platform::Sdl2Application::keyPressEvent()} */
+        void keyPressEvent(ApplicationBase::KeyEvent& event) override;
+
+        /** @seemagnum{Platform::Sdl2Application::keyReleaseEvent()} */
+        void keyReleaseEvent(ApplicationBase::KeyEvent& event) override;
+
+        /** @seemagnum{Platform::Sdl2Application::mousePressEvent()} */
+        void mousePressEvent(ApplicationBase::MouseEvent& event) override;
+
+        /** @seemagnum{Platform::Sdl2Application::mouseReleaseEvent()} */
+        void mouseReleaseEvent(ApplicationBase::MouseEvent& event) override;
+
+        /** @seemagnum{Platform::Sdl2Application::mouseMoveEvent()} */
+        void mouseMoveEvent(ApplicationBase::MouseMoveEvent& event) override;
+};
+
+}
+
+#endif
