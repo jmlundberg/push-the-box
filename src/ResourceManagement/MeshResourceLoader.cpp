b3a7d164cac35ebd6cbbde340fdd80b186fd10ff
Vladimír Vondruš <mosra@centrum.cz>
2012-11-25 02:51:52 +0100

    Initial version of mesh resource loader.

---
+++ src/ResourceManagement/MeshResourceLoader.cpp
@@ -0,0 +1,82 @@
+#include "MeshResourceLoader.h"
+
+#include <Utility/Resource.h>
+#include <Buffer.h>
+#include <IndexedMesh.h>
+#include <Shaders/PhongShader.h>
+
+#include <Resource.h>
+
+using Corrade::Utility::Configuration;
+using Corrade::Utility::ConfigurationGroup;
+
+namespace PushTheBox { namespace ResourceManagement {
+
+MeshResourceLoader::MeshResourceLoader() {
+    /* Get data from compiled-in resource */
+    /** @todo Configuration directly from string, data as raw pointer */
+    Corrade::Utility::Resource rs("PushTheBoxData");
+    std::istringstream in(rs.get("push-the-box.conf"));
+    conf = new Configuration(in, Configuration::ReadOnly);
+    data = rs.get("push-the-box.mesh");
+
+    /* Fill name map */
+    for(size_t i = 0, end = conf->groupCount("mesh"); i != end; ++i)
+        nameMap[conf->group("mesh", i)->value<std::string>("name")] = i;
+}
+
+std::string MeshResourceLoader::name(ResourceKey key) const {
+    auto it = nameMap.find(key);
+    if(it == nameMap.end()) return "";
+    return conf->group("mesh", it->second)->value<std::string>("name");
+}
+
+void MeshResourceLoader::load(ResourceKey key) {
+    AbstractResourceLoader<Mesh>::load(key);
+
+    auto it = nameMap.find(key);
+    ConfigurationGroup* group;
+    if(it == nameMap.end() || !(group = conf->group("mesh", it->second))) {
+        Warning() << "Resource" << key << "('" + name(key) + "') was not found";
+        setNotFound(key);
+        return;
+    }
+
+    /* Indexed mesh */
+    Mesh* mesh;
+    if(group->keyExists("indexOffset")) {
+        IndexedMesh* indexedMesh = new IndexedMesh;
+        mesh = indexedMesh;
+
+        /* Add index buffer to the manager */
+        Buffer* indexBuffer = new Buffer(Buffer::Target::ElementArray);
+        SceneResourceManager::instance()->set(group->value("name") + "-index", indexBuffer, ResourceDataState::Final, ResourcePolicy::Resident);
+
+        /* Configure indices */
+        indexedMesh->setIndexBuffer(indexBuffer)
+                   ->setIndexCount(group->value<GLsizei>("indexCount"))
+                   ->setIndexType(group->value<Type>("indexType"));
+        indexBuffer->setData(indexedMesh->indexCount()*TypeInfo::sizeOf(indexedMesh->indexType()),
+                             data.c_str()+group->value<std::size_t>("indexOffset"),
+                             Buffer::Usage::StaticDraw);
+
+    /* Non-indexed mesh */
+    } else mesh = new Mesh;
+
+    /* Add vertex buffer to the manager */
+    Buffer* vertexBuffer = new Buffer;
+    SceneResourceManager::instance()->set(group->value("name") + "-vertex", vertexBuffer, ResourceDataState::Final, ResourcePolicy::Resident);
+
+    /* Configure vertices */
+    mesh->setPrimitive(group->value<Mesh::Primitive>("primitive"))
+        ->setVertexCount(group->value<GLsizei>("vertexCount"))
+        ->addInterleavedVertexBuffer(vertexBuffer, 0, Shaders::PhongShader::Position(), Shaders::PhongShader::Normal());
+    vertexBuffer->setData(mesh->vertexCount()*group->value<std::size_t>("vertexStride"),
+                          data.c_str()+group->value<std::size_t>("vertexOffset"),
+                          Buffer::Usage::StaticDraw);
+
+    /* Finally add the mesh to the manager */
+    set(key, mesh, ResourceDataState::Final, ResourcePolicy::Resident);
+}
+
+}}
---
+++ src/ResourceManagement/MeshResourceLoader.h
@@ -0,0 +1,38 @@
+#ifndef PushTheBox_ResourceManagement_MeshResourceLoader_h
+#define PushTheBox_ResourceManagement_MeshResourceLoader_h
+
+#include <Utility/Configuration.h>
+#include <AbstractResourceLoader.h>
+
+#include "PushTheBox.h"
+
+namespace PushTheBox { namespace ResourceManagement {
+
+#ifndef DOXYGEN_GENERATING_OUTPUT
+namespace Implementation {
+    struct ResourceKeyHash {
+        inline std::size_t operator()(ResourceKey key) const {
+            return *reinterpret_cast<const std::size_t*>(key.byteArray());
+        }
+    };
+}
+#endif
+
+class MeshResourceLoader: public AbstractResourceLoader<Mesh> {
+    public:
+        MeshResourceLoader();
+
+        std::string name(ResourceKey key) const;
+
+        void load(ResourceKey key) override;
+
+    private:
+        std::unordered_map<ResourceKey, std::uint32_t, Implementation::ResourceKeyHash> nameMap;
+
+        Corrade::Utility::Configuration* conf;
+        std::string data;
+};
+
+}}
+
+#endif
