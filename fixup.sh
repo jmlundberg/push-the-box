0972026d2058dae0e9bf4c84da0794bed821c8f1
Vladimír Vondruš <mosra@centrum.cz>
2013-07-06 00:47:15 +0200

    Updated to 8ad47e5e69822f0a2352d562a8f277bbed9aa411.
    Using newlib instead of glibc, the binary has now only 3.7 MB instead of
    10. For single game it is the best, for more games sharing common code
    not so much.

--- fixup.sh
+++ fixup.sh
@@ -1,6 +1,1 @@
-mv -f x86-32/libCorradePluginManager.so.0{.2,}
-mv -f x86-32/libCorradeUtility.so.0{.2,}
-mv -f x86-64/libCorradePluginManager.so.0{.2,}
-mv -f x86-64/libCorradeUtility.so.0{.2,}
-
-/usr/nacl/toolchain/linux_x86_glibc/bin/x86_64-nacl-strip x86-64/* x86-32/* *.nexe
+/usr/nacl/toolchain/linux_x86_glibc/bin/x86_64-nacl-strip *.nexe
